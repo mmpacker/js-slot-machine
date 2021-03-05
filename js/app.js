920a2e991c86da863f6a714c19665d10d31626ec
mmpacker <mmpacker@gmail.com>
2021-03-05 14:51:14 -0500

    More CSS work. Started outline of JS.

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// JS Slot Machine by Matt Packer
+
+// CONSTANTS
+
+const playIcon = {
+    'bar': '➖',
+    'cherry': '🍒',
+    'bell': '🔔',
+    'diamond': '💎',
+    'seven': '7️⃣',
+}
+
+
+// VARIABLES (state)
+let reels = [];
+let isWinner = '';
+
+
+// CACHED ELEMENT REFERENCES
+const reel1EL = document.getElementById('reel1');
+const reel2El = document.getElementById('reel2');
+const reel3El = document.getElementById('reel3');
+
+let messageEl = document.getElementById('slotMsg');
+
+// EVENT LISTENERS
+
+
+// FUNCTIONS
+
+// Initialize
+
+
+// Click Handler
+
+
+// Check Winner
+
+
+// Render
+
+
+// INVOKE INIT
