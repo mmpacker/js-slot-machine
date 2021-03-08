c2776e3fb6f70e2688c1640743b66034ed7e1a58
mmpacker <mmpacker@gmail.com>
2021-03-07 23:35:29 -0500

    Rewrote JS to fix control flow. Added spin animation. Added reel render delays using setTimeout.

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # JS Slot Machine
 ### by Matt Packer
 
+![JS Slot Machine Wireframe] (planning/JS Slot Machine-Wireframe_v2.png)
+
+## Technical Summary
+
+* Technology used in development:
+  * Javascript
+  * HTML
+  * CSS
+  * Git
+  * Github
+
+## App Requirement Summary
+
+
+## Game Summary
+
+
+## Pseudocode
 
 
--- js/app2.js
+++ js/app2.js
@@ -2,28 +2,23 @@
 
 // CONSTANTS
 
-const eTankImg = 'images/eTankGif_resize.gif';  //E-Tank - 50
-const metImg = 'images/metGif2_resize.gif'; // Met - 100
-const rushImg = 'images/rushCoilGif_resize.gif'; // Rush - 250
-const wileyImg = 'images/wileyGif_resize.gif'; // Dr. Wiley - 500
-const mmImg = 'images/mmGif_resize.gif'; // Mega Man - 1000 (Jackpot)
-
+const img1 = 'images/eTankGif_resize.gif';  //E-Tank - 50
+const img2 = 'images/metGif2_resize.gif'; // Met - 100
+const img3 = 'images/rushCoilGif_resize.gif'; // Rush - 250
+const img4 = 'images/wileyGif_resize.gif'; // Dr. Wiley - 500
+const img5 = 'images/mmGif_resize.gif'; // Mega Man - 1000 (Jackpot)
 const imageDefault1 = 'images/mm1Up_resize.gif';
 const imageDefault2 = 'images/mmTitle_resize.gif';
 
-const spinImg = 'images/mmSpin_resize.gif'
 
 // VARIABLES (state)
 
-let reel1State = '';
-let reel2State = '';
-let reel3State = '';
-
+let re1 = null;
+let re2 = null;
+let re3 = null;
 let isWinner = '';
-
 let winCredits = 0;
-let remainCredits = 0;
-
+let playerCredits = 0;
 let randNum1 = null;
 let randNum2 = null;
 let randNum3 = null;
@@ -35,14 +30,14 @@ const slotEl1 = document.getElementById('el1');
 const slotEl2 = document.getElementById('el2');
 const slotEl3 = document.getElementById('el3');
 
-const winCrEl = document.getElementById('crWon')
-const remainCrEl = document.getElementById('crRemain')
-const messageEl = document.getElementById('slotMsg');
+let winCrEl = document.getElementById('crWon')
+let crLeftEl = document.getElementById('crLeft')
+let messageEl = document.getElementById('slotMsg');
 
 
 // EVENT LISTENERS
 
-document.getElementById('spinBtn').addEventListener('click', spin)
+document.getElementById('spinBtn').addEventListener('click', spinReels)
 
 document.getElementById('resetBtn').addEventListener('click', init)
 
@@ -51,189 +46,182 @@ document.getElementById('resetBtn').addEventListener('click', init)
 
 // Initialize
 function init(){
-    reel1State = imageDefault1;
-    reel2State = imageDefault2;
-    reel3State = imageDefault1;
-    slotEl1.src = reel1State;
-    slotEl2.src = reel2State;
-    slotEl3.src = reel3State;
-    
+    re1 = null;
+    re2 = null;
+    re3 = null;
     isWinner = null;
-    remainCredits = 100;
+    playerCredits = 100;
     winCredits = 0;
     messageEl.innerText = `Press SPIN to Play!`;
-    remainCrEl.innerText = remainCredits;
-    winCrEl.innerText = winCredits;
-    
+    crLeftEl.innerText = playerCredits;
+    winCredits.innerText = winCredits;
+    slotEl1.src = imageDefault1;
+    slotEl2.src = imageDefault1;
+    slotEl3.src = imageDefault1;
     randNum1 = null;
     randNum2 = null;
     randNum3 = null;
 }
 
 // Click Handler
-function spin(){
-    if (remainCredits <= 0) {
+function spinReels(){
+    if (playerCredits <= 0) {
         messageEl.innerText = `Game Over! Press RESET to play again!`
     } else {
-        remainCredits = (remainCredits - 1);
+        playerCredits = (playerCredits - 1);
         winCredits = 0;
-        
         isWinner = '';
-        
         messageEl.innerText = `Reels Spinning...`;
-        
-        reel1State = spinImg;
-        reel2State = spinImg;
-        reel3State = spinImg;
-        slotEl1.src = reel1State;
-        slotEl2.src = reel2State;
-        slotEl3.src = reel3State;
-
-        spinReel1();
-        spinReel2();
-        spinReel3();
-
-        ckWinner();
+        re1 = null;
+        re2 = null;
+        re3 = null;
+        console.log(playerCredits);
+        render();
     }
 }
 
 // Spin Reels
 
 function spinReel1(){ 
+    let result1;
     randNum1 = Math.floor(Math.random() * ((100-1)+1)) + 1;
     if(randNum1 >= 1 && randNum1 <= 50){
-        reel1State = eTankImg
+        result1 = img1
     } else if(randNum1 >= 51 && randNum1 <= 65) {
-        reel1State = metImg
+        result1 = img2
     } else if(randNum1 >= 66 && randNum1 <= 80) {
-        reel1State = rushImg
+        result1 = img3
     } else if (randNum1 >= 81 && randNum1 <= 90) {
-        reel1State = wileyImg
+        result1 = img4
     } else if (randNum1 >= 91 && randNum1 <= 100) {
-        reel1State = mmImg
+        result1 = img5
     }
+    return result1;
 }
-
+setTimeout(spinReel1, 2000)
 
 function spinReel2(){ 
+    let result2;
     randNum2 = Math.floor(Math.random() * ((100-1)+1)) + 1;
     if(randNum2 >= 1 && randNum2 <= 75){
-        reel2State = eTankImg
+        result2 = img1
     } else if(randNum2 >= 76 && randNum2 <= 90) {
-        reel2State = metImg
+        result2 = img2
     } else if(randNum2 >= 91 && randNum2 <= 95) {
-        reel2State = rushImg
+        result2 = img3
     } else if (randNum2 >= 96 && randNum2 <= 98) {
-        reel2State = wileyImg
+        result2 = img4
     } else if (randNum2 >= 99 && randNum2 <= 100) {
-        reel2State = mmImg
+        result2 = img5
     }
+    return result2;
 }
+setTimeout(spinReel2, 2000)
 
 function spinReel3(){ 
+    let result3;
     randNum3 = Math.floor(Math.random() * ((100-1)+1)) + 1;
     if(randNum3 >= 1 && randNum3 <= 75){
-        reel3State = eTankImg
+        result3 = img1
     } else if(randNum3 >= 76 && randNum3 <= 90) {
-        reel3State = metImg
+        result3 = img2
     } else if(randNum3 >= 91 && randNum3 <= 95) {
-        reel3State = rushImg
+        result3 = img3
     } else if (randNum3 >= 96 && randNum3 <= 98) {
-        reel3State = wileyImg
+        result3 = img4
     } else if (randNum3 >= 99 && randNum3 <= 100) {
-        reel3State = mmImg
+        result3 = img5
     }
+    return result3;
 }
+setTimeout(spinReel3, 2000)
 
 
 // Check Winner
 function ckWinner(){
-    if(reel1State === reel2State && reel2State === reel3State && reel3State === mmImg) {
+    if(re1 === re2 && re2 === re3 && re3 === img5) {
         isWinner = 'J'
-    } else if (reel1State === reel2State && reel2State === reel3State && reel3State === wileyImg) {
+    } else if (re1 === re2 && re2 === re3 && re3 === img4) {
         isWinner = 'W4'
-    } else if (reel1State === reel2State && reel2State === reel3State && reel3State === rushImg) {
+    } else if (re1 === re2 && re2 === re3 && re3 === img3) {
         isWinner = 'W3'
-    } else if (reel1State === reel2State && reel2State === reel3State && reel3State === metImg) {
+    } else if (re1 === re2 && re2 === re3 && re3 === img2) {
         isWinner = 'W2'
-    } else if (reel1State === reel2State && reel2State === reel3State && reel3State === eTankImg) {
+    } else if (re1 === re2 && re2 === re3 && re3 === img1) {
         isWinner = 'W1'
     } else {
         isWinner = 'L'
     }
-    setTimeout(renderReel1, 1500);
+    console.log(isWinner)
 }
 
 // Render
+function render(){
+    re1 = spinReel1();
+    slotEl1.src = re1
 
-function renderReel1(){
-    slotEl1.src = reel1State
-    setTimeout(renderReel2, 1250)
-}
+    re2 = spinReel2();
+    slotEl2.src = re2
 
-function renderReel2(){
-    slotEl2.src = reel2State
-    setTimeout(renderReel3, 1250)
-}
+    re3 = spinReel3();
+    slotEl3.src = re3
 
-function renderReel3(){
-    slotEl3.src = reel3State
-    setTimeout(renderFinal, 250)
-}
-
-function renderFinal(){
+    ckWinner();
+    
     if(isWinner === 'J') {
         messageEl.innerText = `MEGA JACKPOT WINNER!!! 🎉 🤖 🎉`
         
         winCredits = 1000
         winCrEl.innerText = winCredits
 
-        remainCredits = (remainCredits + winCredits)
-        remainCrEl.innerText = remainCredits
+        playerCredits = (playerCredits + winCredits)
+        crLeftEl.innerText = playerCredits
     } else if (isWinner === 'W4') {
         messageEl.innerText = `Triple Wiley Winner! 💀`
         
         winCredits = 500
         winCrEl.innerText = winCredits
 
-        remainCredits = (remainCredits + winCredits)
-        remainCrEl.innerText = remainCredits
+        playerCredits = (playerCredits + winCredits)
+        crLeftEl.innerText = playerCredits
     } else if (isWinner === 'W3') {
         messageEl.innerText = `Triple Rush Winner! 🐕`
          
         winCredits = 250
         winCrEl.innerText = winCredits
 
-        remainCredits = (remainCredits + winCredits)
-        remainCrEl.innerText = remainCredits
+        playerCredits = (playerCredits + winCredits)
+        crLeftEl.innerText = playerCredits
     } else if (isWinner === 'W2') {
         messageEl.innerText = `You Win! 💯`
         
         winCredits = 100
         winCrEl.innerText = winCredits
 
-        remainCredits = (remainCredits + winCredits)
-        remainCrEl.innerText = remainCredits
+        playerCredits = (playerCredits + winCredits)
+        crLeftEl.innerText = playerCredits
     } else if (isWinner === 'W1') {
         messageEl.innerText = `Winner!`
         
         winCredits = 50
         winCrEl.innerText = winCredits
 
-        remainCredits = (remainCredits + winCredits)
-        remainCrEl.innerText = remainCredits
+        playerCredits = (playerCredits + winCredits)
+        crLeftEl.innerText = playerCredits
     } else {
         winCredits = 0
         winCrEl.innerText = winCredits
 
-        remainCredits = (remainCredits + winCredits)
-        remainCrEl.innerText = remainCredits
-        if(remainCredits > 0) {
+        playerCredits = (playerCredits + winCredits)
+        crLeftEl.innerText = playerCredits
+        if(playerCredits > 0) {
             messageEl.innerText = `Press SPIN to try again!`
         } else {
             messageEl.innerText = `Game Over! Press RESET to play again!`
         }
     }
+    console.log(playerCredits)
+    console.log(winCredits)
 };
 
 // INVOKE INIT
